a8badcfb9c421f7258cf5615da0e78d57c649ea5
Justin Emgarten <justin@emgarten.com>
2016-07-26 23:39:02 -0700

    Adding restore task

---
+++ src/NuGet.BuildTasks/MSBuildLogger.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Build.Utilities;
+using NuGet.Common;
+
+namespace NuGet.BuildTasks
+{
+    /// <summary>
+    /// TaskLoggingHelper -> ILogger
+    /// </summary>
+    internal class MSBuildLogger : ILogger
+    {
+        private readonly TaskLoggingHelper _taskLogging;
+
+        public MSBuildLogger(TaskLoggingHelper taskLogging)
+        {
+            _taskLogging = taskLogging;
+        }
+
+        public void LogDebug(string data)
+        {
+            _taskLogging.LogMessage(data);
+        }
+
+        public void LogError(string data)
+        {
+            _taskLogging.LogError(data);
+        }
+
+        public void LogErrorSummary(string data)
+        {
+            _taskLogging.LogMessage(data);
+        }
+
+        public void LogInformation(string data)
+        {
+            _taskLogging.LogMessage(data);
+        }
+
+        public void LogInformationSummary(string data)
+        {
+            _taskLogging.LogMessage(data);
+        }
+
+        public void LogMinimal(string data)
+        {
+            _taskLogging.LogMessage(data);
+        }
+
+        public void LogVerbose(string data)
+        {
+            _taskLogging.LogMessage(data);
+        }
+
+        public void LogWarning(string data)
+        {
+            _taskLogging.LogWarning(data);
+        }
+    }
+}
---
+++ src/NuGet.BuildTasks/RestoreTask.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Commands;
+using NuGet.Configuration;
+using NuGet.ProjectModel;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+
+namespace NuGet.BuildTasks
+{
+    /// <summary>
+    /// .NET Core compatible restore task for csproj + project.json.
+    /// </summary>
+    public class RestoreTask : Task
+    {
+        /// <summary>
+        /// DG file entries
+        /// </summary>
+        [Required]
+        public ITaskItem[] RestoreGraphItems { get; set; }
+
+        public override bool Execute()
+        {
+            var log = new MSBuildLogger(Log);
+
+            using (var cacheContext = new SourceCacheContext())
+            {
+                cacheContext.NoCache = false;
+                cacheContext.IgnoreFailedSources = false;
+                var providerCache = new RestoreCommandProvidersCache();
+
+                // Ordered request providers
+                var providers = new List<IRestoreRequestProvider>();
+                providers.Add(new MSBuildP2PRestoreRequestProvider(providerCache));
+
+                ISettings defaultSettings = Settings.LoadDefaultSettings(root: null, configFileName: null, machineWideSettings: null);
+                CachingSourceProvider sourceProvider = new CachingSourceProvider(new PackageSourceProvider(defaultSettings));
+
+                var restoreContext = new RestoreArgs()
+                {
+                    CacheContext = cacheContext,
+                    LockFileVersion = LockFileFormat.Version,
+                    ConfigFile = null,
+                    DisableParallel = false,
+                    GlobalPackagesFolder = null,
+                    Inputs = new List<string>(),
+                    Log = log,
+                    MachineWideSettings = new XPlatMachineWideSetting(),
+                    RequestProviders = providers,
+                    Sources = new List<string>(),
+                    FallbackSources = new List<string>(),
+                    CachingSourceProvider = sourceProvider
+                };
+
+                if (restoreContext.DisableParallel)
+                {
+                    HttpSourceResourceProvider.Throttle = SemaphoreSlimThrottle.CreateBinarySemaphore();
+                }
+
+                var restoreSummaries = RestoreRunner.Run(restoreContext).Result;
+
+                // Summary
+                RestoreSummary.Log(log, restoreSummaries);
+
+                return restoreSummaries.All(x => x.Success);
+            }
+        }
+    }
+}
